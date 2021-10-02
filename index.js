93418808d727438c3daa43c136356a8148160466
Bilal Ahmad <bilal.ahmad0796@gmail.com>
2021-10-02 21:34:49 +0200

    new files created, html and css
    index.html represents now the main page while quiz.html is the game page . and same for css

--- index.js
+++ index.js
@@ -110,6 +110,9 @@ let questions = [
   ];
 //   let questionscontainerNode = document.getElementById("QuestionsContainer")
   const displayquestions = function(){
+    // let changetitle=document.getElementsByTagName("h1")
+    // changetitle.innerText="Good Lcuk with the game"
+    // document.body.appendChild(changetitle)
     let gettheclickeddata
     let questionscontainerNode = document.getElementById("QuestionsContainer")
       for(let i = 0; i <= 9; i++){
@@ -157,21 +160,21 @@ let questions = [
 
   }
 
-window.onload = function () {
-    displayquestions()
-    // HINTS
-    // IF YOU ARE DISPLAYING ALL THE QUESTIONS AT ONCE:
-    // For each question, create a container for wrapping it; then create a radio button
-    // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/radio
-    // with, as options, both the correct answer and the incorrect ones
-    // (you'll probably need to google how to get the value from a radio button in JS to evaluate the final score)
-    //
-    // IF YOU ARE DISPLAYING ONE QUESTION AT A TIME
-    // Display the first question with the text and the radio buttons
-    // when the user selects an answer, pick the next question from the array and replace the old one with it
-    // saving the user's choice in a variable
+// window.onload = function () {
+//     displayquestions()
+//     // HINTS
+//     // IF YOU ARE DISPLAYING ALL THE QUESTIONS AT ONCE:
+//     // For each question, create a container for wrapping it; then create a radio button
+//     // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/radio
+//     // with, as options, both the correct answer and the incorrect ones
+//     // (you'll probably need to google how to get the value from a radio button in JS to evaluate the final score)
+//     //
+//     // IF YOU ARE DISPLAYING ONE QUESTION AT A TIME
+//     // Display the first question with the text and the radio buttons
+//     // when the user selects an answer, pick the next question from the array and replace the old one with it
+//     // saving the user's choice in a variable
     
-      // How to calculate the result? You can do it in 2 ways:
-      // If you are presenting all the questions together, just take all the radio buttons and check if the selected answer === correct_answer
-      // If you are presenting one question at a time, just add one point or not to the user score if the selected answer === correct_answer
-}
+//       // How to calculate the result? You can do it in 2 ways:
+//       // If you are presenting all the questions together, just take all the radio buttons and check if the selected answer === correct_answer
+//       // If you are presenting one question at a time, just add one point or not to the user score if the selected answer === correct_answer
+// }
